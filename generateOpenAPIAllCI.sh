62596256fbb5b6391c7a4db7c1d35e36cf2cc877
Felix Winterhalter <felix.winterhalter@fau.de>
2021-03-19 11:39:22 +0100

    Fix ci generate file

--- generateOpenAPIAllCI.sh
+++ generateOpenAPIAllCI.sh
@@ -1,13 +1,13 @@
 /usr/local/bin/docker-entrypoint.sh \
 generate \
-    -i ${PWD}/Clean.Architecture.ApiSpec.yaml \
+    -i ${PWD}/Clean.Architecture.ApiSpec.yml \
     -g aspnetcore \
     -o ${PWD}/ \
     -c ${PWD}/openAPIServerGeneratorConfig.yml
 
 /usr/local/bin/docker-entrypoint.sh \
 generate \
-    -i ${PWD}/Clean.Architecture.ApiSpec.yaml \
+    -i ${PWD}/Clean.Architecture.ApiSpec.yml \
     -g csharp-netcore \
     -o ${PWD}/ \
     -c ${PWD}/openAPIClientGeneratorConfig.yml
